662a95d571ba4ebe30dc1a27c952110755c929c8
osama <147931378+aboalos@users.noreply.github.com>
2025-05-27 16:15:18 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -7,11 +7,11 @@ var system = System();
 
 var emp1 = Employee(name: "Osama", id: 1, hourIn: "7AM", hourOut: "5PM");
 var emp2 = Employee(name: "Ali", id: 2, hourIn: "8AM", hourOut: "4PM");
-var sup1 = Supervisor(name: "Mr. Ahmad", id: 101);
+var sup1 = Supervisor(name: "Mr. Ahmad", id: 1);
 
 system.addemployee(emp1);
 system.addemployee(emp2);
-//system.addvisor(sup1);
+system.addvisor(sup1);
 
 system.assignEmployee(sup1, emp1);
 system.assignEmployee(sup1, emp2);
